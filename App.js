7fbe6de2c9559dbef0a380f1de2db797d3b31b7a
gphir704 <giorgi.fircxo1997@gmail.com>
2023-12-30 14:29:27 +0400

    first

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  SafeAreaView,
+} from "react-native";
+let id = 0;
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    id++;
+    const text = `TODO NUM ${id}`;
+    setTodos([...todos, { id, text, checked: false }]);
+  };
+  const onDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="addTodo" onPress={addTodo} />
+      <ScrollView style={styles.todoContainer}>
+        {todos.map((task) => (
+          <Text key={task.id} style={styles.todoItem}>
+            {task.text}
+            <Button
+              title="Delete"
+              onPress={() => onDelete(task.id)}
+              style={styles.check}
+            />
+
+            <Button
+              title={task.checked ? "checked" : "check"}
+              onPress={() => onToggle(task.id)}
+            />
+          </Text>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  todoContainer: {
+    borderWidth: 1,
+    width: "90%",
+  },
+  todoItem: {
+    borderWidth: 1,
+    width: "90%",
+    alignSelf: "center",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 20,
+  },
+  check: {
+    borderWidth: 2,
+    borderColor: "red",
+    backgroundColor: "green",
+    borderRadius: 20,
+    width: 10,
   },
 });
